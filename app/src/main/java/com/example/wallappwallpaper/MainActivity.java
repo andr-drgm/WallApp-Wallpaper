0e7d38feebc2ae97c248831e3558a1099aa10506
Horea Dan <danhorea2010@gmail.com>
2020-09-23 14:32:37 +0300

    Added dark theme

--- app/src/main/java/com/example/wallappwallpaper/MainActivity.java
+++ app/src/main/java/com/example/wallappwallpaper/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.UiModeManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
@@ -29,7 +31,15 @@ public class MainActivity extends AppCompatActivity {
         testDB.Add(new WallPaper(R.drawable.wall1,"Author", "Descriere 1", "Titlu fain", "Nume1"));
         testDB.Add(new WallPaper(R.drawable.wall2,"Author2", "Descriere 2", "Titlu mai fain", "Nume2"));
         testDB.Add(new WallPaper(R.drawable.wall3,"Author3", "Descriere 3", "Titlu mult mai fain", "Nume3"));
-        testDB.Add(new WallPaper(R.drawable.wall4,"Author", "Descriere 4", "Titlu ceva acolo", "Nume4"));
+        testDB.Add(new WallPaper(R.drawable.wall4,"Author4", "Descriere 4", "Titlu ceva acolo", "Nume4"));
+        testDB.Add(new WallPaper(R.drawable.wall5,"Author5", "Descriere 4", "Titlu ceva acolo", "Nume4"));
+        testDB.Add(new WallPaper(R.drawable.wall6,"Author6", "Descriere 4", "Titlu ceva acolo", "Nume4"));
+        //testDB.Add(new WallPaper(R.drawable.wall7,"Author", "Descriere 4", "Titlu ceva acolo", "Nume4"));
+        //testDB.Add(new WallPaper(R.drawable.wall8,"Author", "Descriere 4", "Titlu ceva acolo", "Nume4"));
+        //testDB.Add(new WallPaper(R.drawable.wall9,"Author", "Descriere 4", "Titlu ceva acolo", "Nume4"));
+
+        UiModeManager uiManager = (UiModeManager) getApplicationContext().getSystemService(Context.UI_MODE_SERVICE);
+        uiManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
 
         recyclerView = (RecyclerView) findViewById(R.id.wallpapers_list);
         recyclerView.setHasFixedSize(true);
--- app/src/main/java/com/example/wallappwallpaper/WallPaperActivity.java
+++ app/src/main/java/com/example/wallappwallpaper/WallPaperActivity.java
@@ -47,7 +47,6 @@ public class WallPaperActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
     @Override
