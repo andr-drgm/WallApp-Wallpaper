93bc839b0fdc3c984dda9b1b48ea42b9d1d5e93b
Horea Dan <danhorea2010@gmail.com>
2020-09-23 14:35:13 +0300

    Clean-up

--- app/src/main/java/com/example/wallappwallpaper/MainActivity.java
+++ app/src/main/java/com/example/wallappwallpaper/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter wallPaperAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private WallPaperDB testDB;
-    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +23,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         testDB = new WallPaperDB();
-
-        imageView = findViewById(R.id.imageView);
+        Service wallPaperService = new Service(testDB);
 
         //int imagePath, String author, String description, String title, String name
-        testDB.Add(new WallPaper(R.drawable.wall1,"Author", "Descriere 1", "Titlu fain", "Nume1"));
-        testDB.Add(new WallPaper(R.drawable.wall2,"Author2", "Descriere 2", "Titlu mai fain", "Nume2"));
-        testDB.Add(new WallPaper(R.drawable.wall3,"Author3", "Descriere 3", "Titlu mult mai fain", "Nume3"));
-        testDB.Add(new WallPaper(R.drawable.wall4,"Author4", "Descriere 4", "Titlu ceva acolo", "Nume4"));
-        testDB.Add(new WallPaper(R.drawable.wall5,"Author5", "Descriere 4", "Titlu ceva acolo", "Nume4"));
-        testDB.Add(new WallPaper(R.drawable.wall6,"Author6", "Descriere 4", "Titlu ceva acolo", "Nume4"));
-        //testDB.Add(new WallPaper(R.drawable.wall7,"Author", "Descriere 4", "Titlu ceva acolo", "Nume4"));
-        //testDB.Add(new WallPaper(R.drawable.wall8,"Author", "Descriere 4", "Titlu ceva acolo", "Nume4"));
-        //testDB.Add(new WallPaper(R.drawable.wall9,"Author", "Descriere 4", "Titlu ceva acolo", "Nume4"));
+        wallPaperService.AddWallPaper(R.drawable.wall1,"Author", "Descriere 1", "Titlu fain", "Nume1");
+        wallPaperService.AddWallPaper(R.drawable.wall2,"Author2", "Descriere 2", "Titlu mai fain", "Nume2");
+        wallPaperService.AddWallPaper(R.drawable.wall3,"Author3", "Descriere 3", "Titlu mult mai fain", "Nume3");
+        wallPaperService.AddWallPaper(R.drawable.wall4,"Author4", "Descriere 4", "Titlu ceva acolo", "Nume4");
+        wallPaperService.AddWallPaper(R.drawable.wall5,"Author5", "Descriere 4", "Titlu ceva acolo", "Nume4");
+        wallPaperService.AddWallPaper(R.drawable.wall6,"Author6", "Descriere 4", "Titlu ceva acolo", "Nume4");
 
         UiModeManager uiManager = (UiModeManager) getApplicationContext().getSystemService(Context.UI_MODE_SERVICE);
         uiManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
