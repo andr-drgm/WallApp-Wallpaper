d1b2349ffb2d6dd63f3eb148731913ef6325425d
Horea Dan <danhorea2010@gmail.com>
2020-09-26 17:26:19 +0300

    Changed sharedPreferences name to avoid conflicts

--- app/src/main/java/com/example/wallappwallpaper/AboutActivity.java
+++ app/src/main/java/com/example/wallappwallpaper/AboutActivity.java
@@ -1,15 +1,13 @@
 package com.example.wallappwallpaper;
 
 import android.os.Bundle;
+import android.view.View;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AboutActivity extends AppCompatActivity {
 
