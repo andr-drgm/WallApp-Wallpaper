18da5f2f2cee24275eef6fff06b0e030104a9bad
Horea Dan <danhorea2010@gmail.com>
2020-09-28 17:02:08 +0300

    Fixed swipe gesture

--- app/src/main/java/com/example/wallappwallpaper/OnSwipeTouchListener.java
+++ app/src/main/java/com/example/wallappwallpaper/OnSwipeTouchListener.java
@@ -47,14 +47,7 @@ public class OnSwipeTouchListener implements OnTouchListener {
                         result = true;
                     }
                 }
-                else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                    if (diffY > 0) {
-                        onSwipeBottom();
-                    } else {
-                        onSwipeTop();
-                    }
-                    result = true;
-                }
+
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
